31389e437d3e4bff162278252ea9efc5976f5971
Vladislav Simonenko <vlad.simonenko@itp-it.com>
2023-09-27 14:01:51 +0400

    delete path

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import SvgLoader from "vite-svg-loader";
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [
-      react(),
-      SvgLoader(),
-      {
-        name: "vite-plugin-svg",
-      },
-    ],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/wh1tepythons.github.io/";
-  }
-
-  return config;
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    SvgLoader(),
+    {
+      name: "vite-plugin-svg",
+    },
+  ],
+  base: "/",
 });
