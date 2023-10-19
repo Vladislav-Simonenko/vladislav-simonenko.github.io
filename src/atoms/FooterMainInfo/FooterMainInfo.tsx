621dc20854c34e2acd929cb1221c4e5be45a0791
Vladislav Simonenko <vlad.simonenko@itp-it.com>
2023-10-19 12:05:41 +0400

    change footer info

--- src/atoms/FooterMainInfo/FooterMainInfo.tsx
+++ src/atoms/FooterMainInfo/FooterMainInfo.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./FooterMainInfo.module.scss";
 
 interface IFooterInterface {
@@ -10,8 +11,13 @@ export const FooterMainInfo = (props: IFooterInterface) => {
   return (
     <div className={styles.footerMainInfo}>
       <div className={styles.footerTopMainInfo}>
-        <a href="https://reestr.digital.gov.ru/reestr/1864621/">
-          <p className={styles.footerMainText}>{reestrInfo}</p>
+        <p>{reestrInfo}</p>
+        <a
+          className={styles.footerMainText}
+          href="https://reestr.digital.gov.ru/reestr/1864621/"
+          target="_blank"
+        >
+          Система Zernovoz
         </a>
         <a className={styles.footerMainText} href="/reestr.pdf" target="_blank">
           (выписка из реестра)
@@ -19,18 +25,17 @@ export const FooterMainInfo = (props: IFooterInterface) => {
         <p className={styles.footerMainText}>{policy}</p>
       </div>
       <div className={styles.footerBottomMainInfo}>
-        <p className={styles.footerMainText}>
-          Компания ООО ИТ ПРЕМИУМ{" "}
-          <span>
-            адрес: Ростовская обл., г. Таганрог, ул. Октябрьская, д. 18 каб. 21
-            зарегистрирована 20.08.2018.
-          </span>
-          <span> ИНН 6154153150, ОГРН 1186196034837, КПП 615401001.</span>
-          <span style={{ display: "block" }}>
-            Основным видом деятельности является разработка компьютерного
-            программного обеспечения
-          </span>
-        </p>
+        <Link to={"/contacts"}>
+          <p>
+            Компания ООО ИТ ПРЕМИУМ адрес: Ростовская обл., г. Таганрог, ул.
+            Октябрьская, д. 18 каб. 21 зарегистрирована 20.08.2018.
+            <span> ИНН 6154153150, ОГРН 1186196034837, КПП 615401001.</span>
+            <span style={{ display: "block" }}>
+              Основным видом деятельности является разработка компьютерного
+              программного обеспечения
+            </span>
+          </p>
+        </Link>
       </div>
     </div>
   );
